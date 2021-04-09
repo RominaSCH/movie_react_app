c6f7defe5f7cbd71d00bbd7781329ac002da2273
Romina Schann <cuqi213@gmail.com>
2021-04-09 10:54:44 +0900

    basic

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//react application은 한 번에 하나의 component(App.js)만
+//rendering 할 수 있다.
+//다른건 App.js에 넣음
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
