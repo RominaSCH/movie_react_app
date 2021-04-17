fbf95217bc209749c7b8291c9978e7d4f9613c44
Romina Schann <cuqi213@gmail.com>
2021-04-18 00:15:23 +0900

    prop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
 import React from "react";
 
-function Favorite(props){
-  console.log(props);
-  return <h3>I Like Programming</h3>;
+
+//function name's first alphabet must be upper?
+
+
+// function renderLang(item){
+//   return <Lang name={item.name} image={item.image} />;
+// }
+
+//map function gets array and return array what we want!
+
+const langILike = [
+  {
+    id:1,
+    name:"Python",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png"
+  },
+  {
+    id:2,
+    name:"Javascript",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png"
+  },
+  {
+    id:3,
+    name:"Django",
+    image:"https://blog.kakaocdn.net/dn/cVaSOX/btqD9jVw36X/jHpIEqn2EAk7xdKMMmpEP0/img.png"
+  },
+  {
+    id:4,
+    name:"React",
+    image:"https://www.pinclipart.com/picdir/middle/537-5374089_react-js-logo-clipart.png"
+  },
+  {
+    id:5,
+    name:"Tailwind",
+    image:"https://ichi.pro/assets/images/max/640/1*5QD8DKhOjRe-gcYjozlLNQ.png"
+  }
+];
+
+function Lang({name, image}){ //{name} = props.name
+  return (
+    <div>
+      <span>I like {name}</span>
+      <img src={image} alt={name} />
+    </div>
+  );
 }
 
 function App() {
-  return <div>
-    <h1>Hello World</h1>
-    <Favorite name="python" />
-    </div>
+  return (
+  <div>
+    {langILike.map(item => (
+      <Lang key={item.id} name={item.name} image={item.image} />
+    ))}
+  </div>
+  );
 }
 
 export default App;
