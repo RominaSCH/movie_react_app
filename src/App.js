9ce1d3b1770c5470cb1673664a697600b9b44810
Romina Schann <cuqi213@gmail.com>
2021-04-20 11:37:12 +0900

    async await

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from "react";
+import axios from "axios";
 // import PropTypes from "prop-types";
 
 
 class App extends React.Component{
-    state = { //state 에는 바꾸고 싶은 데이터를 넣는다
-        count:"0",
+    state = { 
+        isLoading: true,
+        movies: [],
     };
-    add = () => {
-        this.setState(current => ({count: current.count +1}));
-        // this.setState({count: this.state.count -1});
-    };
-    minus = () => {
-        this.setState(current => ({count: current.count -1}));
-        // this.setState({count: this.state.count -1});
-        //every time you call setState
-        //react is going to Re render function with the new state
-    };
+    getMovies = async() => { //async 가 있어야 await 이 가능
+        const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+        console.log(movies);
+    }//await이 끝나야 async가 실행될 수 있다?
+    componentDidMount(){ //render 시에 가장 먼저 호출되는 life cycle method
+        this.getMovies();
+    }
+
   render(){
-      return <div>
-          <h1>The number is : {this.state.count}</h1>
-          <button onClick={this.add}>ADD</button>
-          <button onClick={this.minus}>Minus</button>
-      </div>
+      const {isLoading} = this.state;
+      return <div>{this.state.isLoading ? "Loading" : "We are ready"}</div>
   }
 
 }//react automatically is going excuse your render methods in class component
