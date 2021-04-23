95bb72d0b13c851cf212a58d9938709a470f5128
Romina Schann <cuqi213@gmail.com>
2021-04-23 20:57:10 +0900

    genre added

--- src/Movie.js
+++ src/Movie.js
@@ -5,13 +5,19 @@ import "./Movie.css";
 
 //component에 state가 필요 없을 경우는 class component말고
 //function을 써도 된다.
-function Movie({year, title, summary, poster}){
+function Movie({year, title, summary, poster, genres}){
     return (
         <div className="movie">
+          <label htmlFor="movie"></label>
           <img src={poster} alt={title} title={title} />
           <div className="movie__data">
-            <h3 style={{}} className="movie__title">{title} ({year})</h3>
+            <h3 style={{}} className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="genres">{genres.map((genre, index) => (
+              <li key={index} className="genres__genre">{genre}</li>) 
+            )}</ul>
             <p className="movie__summary">{summary}</p>
+            
           </div>
         </div>
       );
@@ -22,7 +28,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
