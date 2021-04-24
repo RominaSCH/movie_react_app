2b9ca8efea4e752844609609df8d783cbb742e4b
Romina Schann <cuqi213@gmail.com>
2021-04-24 21:12:59 +0900

    css

--- src/Movie.js
+++ src/Movie.js
@@ -8,15 +8,15 @@ import "./Movie.css";
 function Movie({year, title, summary, poster, genres}){
     return (
         <div className="movie">
-          <label htmlFor="movie"></label>
+          {/* <label htmlFor="movie"></label> */}
           <img src={poster} alt={title} title={title} />
           <div className="movie__data">
             <h3 style={{}} className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
-            <ul className="genres">{genres.map((genre, index) => (
-              <li key={index} className="genres__genre">{genre}</li>) 
+            <ul className="movie__genres">{genres.map((genre, index) => (
+              <li key={index} className="genres__genre"> {genre}</li>) 
             )}</ul>
-            <p className="movie__summary">{summary}</p>
+            <div className="movie__summary">{summary}</div>
             
           </div>
         </div>
